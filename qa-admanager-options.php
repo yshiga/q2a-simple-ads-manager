fe98d3015b6e69b991b4ccaa0c7fe75e1e7bb02a
EC2 Default User <ec2-user@ip-172-31-29-147.ap-northeast-1.compute.internal>
2016-05-20 05:55:04 +0900

    add randam ad option to ad  after Question

--- qa-admanager-options.php
+++ qa-admanager-options.php
@@ -12,34 +12,36 @@ class pt_qa_simple_admanager {
 	{
 
 		$ok = null;
-		if (qa_clicked('np_q_save_button')) 
+		if (qa_clicked('np_q_save_button'))
 		{
 			qa_opt('pt_q2a_ad_after_question',(bool)qa_post_text('pt_q2a_ad_after_question'));
-			qa_opt('pt_q2a_ad_after_question_codebox', qa_post_text('pt_q2a_ad_after_question_code_field'));   
-			
+			qa_opt('pt_q2a_ad_after_question_codebox_0', qa_post_text('pt_q2a_ad_after_question_code_field_0'));
+			qa_opt('pt_q2a_ad_after_question_codebox_1', qa_post_text('pt_q2a_ad_after_question_code_field_1'));
+			qa_opt('pt_q2a_ad_after_question_codebox_2', qa_post_text('pt_q2a_ad_after_question_code_field_2'));
+
 			qa_opt('pt_q2a_ad_after_menu_bar',(bool)qa_post_text('pt_q2a_ad_after_menu_bar'));
-			qa_opt('pt_q2a_ad_after_menu_bar_codebox', qa_post_text('pt_q2a_ad_after_menu_bar_code_field'));   
-			
+			qa_opt('pt_q2a_ad_after_menu_bar_codebox', qa_post_text('pt_q2a_ad_after_menu_bar_code_field'));
+
 			qa_opt('pt_q2a_ad_after_all_answers',(bool)qa_post_text('pt_q2a_ad_after_all_answers'));
-			qa_opt('pt_q2a_ad_after_all_answers_codebox', qa_post_text('pt_q2a_ad_after_all_answers_code_field'));   
-			
+			qa_opt('pt_q2a_ad_after_all_answers_codebox', qa_post_text('pt_q2a_ad_after_all_answers_code_field'));
+
 			qa_opt('pt_q2a_ad_after_all_questions',(bool)qa_post_text('pt_q2a_ad_after_all_questions'));
-			qa_opt('pt_q2a_ad_after_all_questions_codebox', qa_post_text('pt_q2a_ad_after_all_questions_code_field'));   
-			
+			qa_opt('pt_q2a_ad_after_all_questions_codebox', qa_post_text('pt_q2a_ad_after_all_questions_code_field'));
+
 			qa_opt('pt_q2a_ad_sidebar',(bool)qa_post_text('pt_q2a_ad_sidebar'));
-			qa_opt('pt_q2a_ad_sidebar_codebox', qa_post_text('pt_q2a_ad_sidebar_code_field'));   
-			
+			qa_opt('pt_q2a_ad_sidebar_codebox', qa_post_text('pt_q2a_ad_sidebar_code_field'));
+
 			$ok = qa_lang('admin/options_saved');
 		}
-        
+
 		qa_set_display_rules($qa_content, array(
-				
+
 			'pt_q2a_ad_after_question_code_display' => 'pt_q2a_ad_after_question',
 			'pt_q2a_ad_after_menu_bar_code_display' => 'pt_q2a_ad_after_menu_bar',
 			'pt_q2a_ad_after_all_answers_code_display' => 'pt_q2a_ad_after_all_answers',
 			'pt_q2a_ad_after_all_questions_code_display' => 'pt_q2a_ad_after_all_questions',
 			'pt_q2a_ad_sidebar_code_display' => 'pt_q2a_ad_sidebar',
-				
+
 		));
 
 		$fields = array();
@@ -50,13 +52,31 @@ class pt_qa_simple_admanager {
 			'value' => qa_opt('pt_q2a_ad_after_question'),
 			'tags' => 'NAME="pt_q2a_ad_after_question" ID="pt_q2a_ad_after_question"',
 		);
-		
+
 		$fields[] = array(
 			'id' => 'pt_q2a_ad_after_question_code_display',
-			'label' => 'Paste HTML Ad Code in this box(try 728x90 banner ad)',
+			'label' => 'Paste HTML Ad1 Code in this box(try 728x90 banner ad).',
 			'type' => 'textarea',
-			'value' => qa_opt('pt_q2a_ad_after_question_codebox'),
-			'tags' => 'NAME="pt_q2a_ad_after_question_code_field"',
+			'value' => qa_opt('pt_q2a_ad_after_question_codebox_0'),
+			'tags' => 'NAME="pt_q2a_ad_after_question_code_field_0"',
+            'rows' => 2,
+		);
+
+		$fields[] = array(
+			'id' => 'pt_q2a_ad_after_question_code_display',
+			'label' => 'Paste HTML Ad2 Code in this box(try 728x90 banner ad)',
+			'type' => 'textarea',
+			'value' => qa_opt('pt_q2a_ad_after_question_codebox_1'),
+			'tags' => 'NAME="pt_q2a_ad_after_question_code_field_1"',
+            'rows' => 2,
+		);
+
+		$fields[] = array(
+			'id' => 'pt_q2a_ad_after_question_code_display',
+			'label' => 'Paste HTML Ad3 Code in this box(try 728x90 banner ad)',
+			'type' => 'textarea',
+			'value' => qa_opt('pt_q2a_ad_after_question_codebox_2'),
+			'tags' => 'NAME="pt_q2a_ad_after_question_code_field_2"',
             'rows' => 2,
 		);
 
@@ -66,7 +86,7 @@ class pt_qa_simple_admanager {
 			'value' => qa_opt('pt_q2a_ad_after_menu_bar'),
 			'tags' => 'NAME="pt_q2a_ad_after_menu_bar" ID="pt_q2a_ad_after_menu_bar"',
 		);
-		
+
 		$fields[] = array(
 			'id' => 'pt_q2a_ad_after_menu_bar_code_display',
 			'label' => 'Paste HTML Ad Code in this box(try 728x15 text/link ad)',
@@ -74,7 +94,7 @@ class pt_qa_simple_admanager {
 			'value' => qa_opt('pt_q2a_ad_after_menu_bar_codebox'),
 			'tags' => 'NAME="pt_q2a_ad_after_menu_bar_code_field"',
             'rows' => 2,
-		);		
+		);
 
 		$fields[] = array(
 			'label' => 'Ad after All Answers',
@@ -82,7 +102,7 @@ class pt_qa_simple_admanager {
 			'value' => qa_opt('pt_q2a_ad_after_all_answers'),
 			'tags' => 'NAME="pt_q2a_ad_after_all_answers" ID="pt_q2a_ad_after_all_answers"',
 		);
-		
+
 		$fields[] = array(
 			'id' => 'pt_q2a_ad_after_all_answers_code_display',
 			'label' => 'Paste HTML Ad Code in this box(try 728x90 banner ad)',
@@ -90,15 +110,15 @@ class pt_qa_simple_admanager {
 			'value' => qa_opt('pt_q2a_ad_after_all_answers_codebox'),
 			'tags' => 'NAME="pt_q2a_ad_after_all_answers_code_field"',
             'rows' => 2,
-		);		
-		
+		);
+
 		$fields[] = array(
 			'label' => 'Ad after All Questions',
 			'type' => 'checkbox',
 			'value' => qa_opt('pt_q2a_ad_after_all_questions'),
 			'tags' => 'NAME="pt_q2a_ad_after_all_questions" ID="pt_q2a_ad_after_all_questions"',
 		);
-		
+
 		$fields[] = array(
 			'id' => 'pt_q2a_ad_after_all_questions_code_display',
 			'label' => 'Paste HTML Ad Code in this box(try 728x90 banner ad)',
@@ -106,15 +126,15 @@ class pt_qa_simple_admanager {
 			'value' => qa_opt('pt_q2a_ad_after_all_questions_codebox'),
 			'tags' => 'NAME="pt_q2a_ad_after_all_questions_code_field"',
             'rows' => 2,
-		);		
-		
+		);
+
 		$fields[] = array(
 			'label' => 'Ad after Sidebar',
 			'type' => 'checkbox',
 			'value' => qa_opt('pt_q2a_ad_sidebar'),
 			'tags' => 'NAME="pt_q2a_ad_sidebar" ID="pt_q2a_ad_sidebar"',
 		);
-		
+
 		$fields[] = array(
 			'id' => 'pt_q2a_ad_sidebar_code_display',
 			'label' => 'Paste HTML Ad Code in this box(try 160x90 text/link ad)',
@@ -122,7 +142,7 @@ class pt_qa_simple_admanager {
 			'value' => qa_opt('pt_q2a_ad_sidebar_codebox'),
 			'tags' => 'NAME="pt_q2a_ad_sidebar_code_field"',
             'rows' => 2,
-		);		
+		);
 
 		$fields[] = array(
 			'type' => 'blank',
@@ -130,9 +150,9 @@ class pt_qa_simple_admanager {
 
 		return array(
 			'ok' => ($ok && !isset($error)) ? $ok : null,
-			
+
 			'fields' => $fields,
-			
+
 			'buttons' => array(
 				array(
 				'label' => qa_lang_html('main/save_button'),
@@ -146,4 +166,3 @@ class pt_qa_simple_admanager {
 /*
 	Omit PHP closing tag to help avoid accidental output
 */
-
